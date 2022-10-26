0b83e6d9aa487c3447f8763c4268821e08c8b078
Umair Abbas <windrgames@gmail.com>
2022-10-26 21:12:14 +0500

    Adding polish and animations

--- script.js
+++ script.js
@@ -7,7 +7,20 @@ const checker = new IntersectionObserver((entries) => {
     }
    });
 });
+const waveHand = new IntersectionObserver((entries) => {
+   entries.forEach((entry)=>{
+    if(entry.isIntersecting){
+        entry.target.classList.remove('wave');
+    }else{
+        entry.target.classList.add('wave');
+    }
+   });
+});
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((elem) => {
     checker.observe(elem);
 });
+
+const hand = document.querySelector(".hand");
+waveHand.observe(hand);
+
